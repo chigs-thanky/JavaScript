97232ad2ecb3860a7637fd4aa648ba5873d397a6
chigs-thanky <chigsthanky@outlook.com>
2026-07-19 10:44:00 +0530

    Learning JavaScript

--- DOM+Challenge+Starting+Files/index.js
+++ DOM+Challenge+Starting+Files/index.js
@@ -1,1 +1,4 @@
 document.querySelector("button").style.backgroundColor = "yellow";
+document.querySelector("h1").classList.add("huge");
+document.querySelector("button").classList.toggle("invisible");
+document.querySelector("h2").innerHTML = "Dee Ray"
